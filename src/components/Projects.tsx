2b623a76f2326272e6f37f81173919b2a4fd714c
tygrus <workfile975@gmail.com>
2025-02-13 08:49:48 +0800

    新增 Projects 組件樣式，改善作品展示的佈局與外觀

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,18 @@
 import projectData from '../data/projectData';
+import '../styles/Projects.css';
 
 const Projects = ({ openModal }: { openModal: (image: string) => void }) => (
   <section className="projects">
     <h2>我的作品</h2>
-    {projectData.map((project, index) => (
-      <div className="project" key={index}>
-        <h3>{project.title}</h3>
-        <img src={project.image} alt={project.title} onClick={() => openModal(project.image)} />
-        <p>{project.description}</p>
-      </div>
-    ))}
+    <div className="gallery">
+      {projectData.map((project, index) => (
+        <div className="project" key={index}>
+          <h3>{project.title}</h3>
+          <img src={project.image} alt={project.title} onClick={() => openModal(project.image)} />
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </div>
   </section>
 );
 
