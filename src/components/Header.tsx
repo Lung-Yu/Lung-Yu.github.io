2c11cffb7e13d1eb27dc3d8f98490cb358ca5307
tygrus <workfile975@gmail.com>
2025-02-13 08:22:49 +0800

    重構 Header.tsx，調整結構並新增容器以改善樣式和可讀性

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,24 @@ import '../styles/Header.css'
 
 const Header = () => (
   <header className="header">
-    <div className="logo-container">
-      <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
+    <div className="container">
+      <div className="logo-container">
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>我的個人作品集</h1>
+      <nav>
+        <ul className="nav-list">
+          <li><a href="#about-me">關於我</a></li>
+          <li><a href="#projects">我的作品</a></li>
+          <li><a href="#contact">聯絡我</a></li>
+        </ul>
+      </nav>
     </div>
-    <h1>我的個人作品集</h1>
-    <nav>
-      <ul className="nav-list">
-        <li><a href="#about-me">關於我</a></li>
-        <li><a href="#projects">我的作品</a></li>
-        <li><a href="#contact">聯絡我</a></li>
-      </ul>
-    </nav>
   </header>
 )
 
