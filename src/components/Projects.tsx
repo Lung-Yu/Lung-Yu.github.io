4451aed15e55660a9011627999f6257d69e11c74
tygrus <workfile975@gmail.com>
2025-02-13 08:13:06 +0800

    重構 Projects.tsx，將項目資料移至陣列並使用 map 渲染，簡化組件結構

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,29 @@
+import React from 'react';
+
+const projectData = [
+  {
+    title: '作品一',
+    image: '/src/assets/images/default-images.jpg',
+    description: '這是一個使用 React 和 Vite 開發的範例項目。',
+  },
+  {
+    title: '作品二',
+    image: '/src/assets/images/default-images.jpg',
+    description: '這是一個使用 TypeScript 和 CSS 開發的範例項目。',
+  },
+];
+
 const Projects = ({ openModal }: { openModal: (image: string) => void }) => (
   <section className="projects">
     <h2>我的作品</h2>
-    <div className="project">
-      <h3>作品一</h3>
-      <img src="/src/assets/images/default-images.jpg" alt="作品一" onClick={() => openModal('/src/assets/images/default-images.jpg')} />
-      <p>這是一個使用 React 和 Vite 開發的範例項目。</p>
-    </div>
-    <div className="project">
-      <h3>作品二</h3>
-      <img src="/src/assets/images/default-images.jpg" alt="作品二" onClick={() => openModal('/src/assets/images/default-images.jpg')} />
-      <p>這是一個使用 TypeScript 和 CSS 開發的範例項目。</p>
-    </div>
+    {projectData.map((project, index) => (
+      <div className="project" key={index}>
+        <h3>{project.title}</h3>
+        <img src={project.image} alt={project.title} onClick={() => openModal(project.image)} />
+        <p>{project.description}</p>
+      </div>
+    ))}
   </section>
-)
+);
 
-export default Projects
+export default Projects;
