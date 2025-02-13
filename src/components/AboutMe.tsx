6b5ed7050ab1776951ef5661a8e12835f5c0a8c8
tygrus <workfile975@gmail.com>
2025-02-13 08:24:24 +0800

    新增 AboutMe 組件，包含個人介紹、專業技能及經歷，並新增樣式以改善外觀

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../styles/AboutMe.css';
 
 const AboutMe = () => (
     <section className="about-me">
         <h2>關於我</h2>
-        <p>你好，我是 [你的名字]，一名前端開發者，擅長使用 React 和 TypeScript。</p>
+        <p>你好，我是 [你的名字]，一名資安顧問和前端開發者，擅長使用 React 和 TypeScript。</p>
+        <h3>專業技能</h3>
+        <ul>
+            <li>資安分析與風險評估</li>
+            <li>滲透測試與漏洞修補</li>
+            <li>前端開發 (React, TypeScript, JavaScript, HTML, CSS)</li>
+            <li>後端開發 (Node.js, Express)</li>
+        </ul>
+        <h3>經歷</h3>
+        <ul>
+            <li>資安顧問 - [公司名稱] (20XX - 現在)</li>
+            <li>前端開發者 - [公司名稱] (20XX - 20XX)</li>
+            <li>資安分析師 - [公司名稱] (20XX - 20XX)</li>
+        </ul>
     </section>
 )
 
