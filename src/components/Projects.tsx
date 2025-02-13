1e407b3538d9cf71dd1e402e6c5eecd8ee142f2b
tygrus <workfile975@gmail.com>
2025-02-13 08:11:45 +0800

    更新 Projects.tsx，修正點擊圖片時開啟的模態圖像路徑

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,12 +3,12 @@ const Projects = ({ openModal }: { openModal: (image: string) => void }) => (
     <h2>我的作品</h2>
     <div className="project">
       <h3>作品一</h3>
-      <img src="/src/assets/images/default-images.jpg" alt="作品一" onClick={() => openModal('/src/assets/images/image1.jpg')} />
+      <img src="/src/assets/images/default-images.jpg" alt="作品一" onClick={() => openModal('/src/assets/images/default-images.jpg')} />
       <p>這是一個使用 React 和 Vite 開發的範例項目。</p>
     </div>
     <div className="project">
       <h3>作品二</h3>
-      <img src="/src/assets/images/default-images.jpg" alt="作品二" onClick={() => openModal('/src/assets/images/image2.jpg')} />
+      <img src="/src/assets/images/default-images.jpg" alt="作品二" onClick={() => openModal('/src/assets/images/default-images.jpg')} />
       <p>這是一個使用 TypeScript 和 CSS 開發的範例項目。</p>
     </div>
   </section>
