6baef2f7454be2a2a3a7b977e6445bbf13dfebf7
tygrus <workfile975@gmail.com>
2025-02-23 11:13:14 +0800

    新增證照資料與圖片，包含 ISO 27001、EC-Council Ethical Hacker 及 ISC2 認證，並更新資料結構以包含類別與機構資訊

--- src/data/certificateData.ts
+++ src/data/certificateData.ts
@@ -1,19 +1,43 @@
 export interface Certificate {
-  category: string;
   title: string;
+  institution: string;
+  category: string;
   image: string;
   description: string;
 }
 
-const certificateData: Certificate[] = [];
+const certificateData: Certificate[] = [
+  {
+    title: 'Ethical Hacking',
+    category: 'Cyber Security',
+    institution: 'EC-Council',
+    image: '/src/assets/images/certifications/ec-council/ECC_Certified Ethical Hacker.jpg',
+    description: 'Certified Ethical Hacker (CEH) is a qualification obtained by demonstrating knowledge of assessing the security of computer systems by looking for weaknesses and vulnerabilities in target systems, using the same knowledge and tools as a malicious hacker, but in a lawful and legitimate manner to assess the security posture of a target system.',
+  },
+  {
+    title: 'ISO 27001:2022 Lead Auditor',
+    category: 'Cyber Security',
+    institution: 'BSI',
+    image: '/src/assets/images/certifications/ISO/ISO_27001_2022_LA.png',
+    description: 'The ISO 27001 Lead Auditor training course will equip participants with the skills and ability to perform audits by applying widely recognized audit principles, procedures and techniques. During this training course, participants will acquire the knowledge and skills to plan and carry out internal and external audits in compliance with ISO 19011 and ISO/IEC 17021-1 certification process.',
+  },
+  {
+    title: 'CSSLP',
+    category: 'Cyber Security',
+    institution: 'ISC2',
+    image: '/src/assets/images/certifications/isc2/isc_csslp.png',
+    description: 'The Certified Secure Software Lifecycle Professional (CSSLP) is a certification designed to ensure that security is considered throughout the entire software development lifecycle. CSSLPs are trained in application security, security design principles, and compliance issues.',
+  },
+  {
+    title: 'CISSP',
+    category: 'Cyber Security',
+    institution: 'ISC2',
+    image: '/src/assets/images/certifications/isc2/isc_cissp.png',
+    description: 'The Certified Information Systems Security Professional (CISSP) is a certification for information technology security professionals. It was created by the International Information Systems Security Certification Consortium (ISC)², an international non-profit specializing in managing security certifications.',
+  }
 
-for (let i = 1; i <= 50; i++) {
-  certificateData.push({
-    title: `Certificate ${i}`,
-    category: i % 2 === 0 ? 'Even' : 'Odd',
-    image: '/src/assets/images/default-images.jpg',
-    description: `Description of certificate ${i}`,
-  });
-}
+
+];
+
 
 export default certificateData;
