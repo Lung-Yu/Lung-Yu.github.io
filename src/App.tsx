9a4b432e737272c30aff7ae259929805840986bd
tygrus <workfile975@gmail.com>
2025-02-24 00:14:37 +0800

    重構樣式系統，新增 CSS 變數以改善可維護性，調整各元件的樣式與佈局，並優化響應式設計

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Certificates from './components/Certificates';
 import Modal from './components/modal/Modal';
