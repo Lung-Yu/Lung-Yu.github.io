402371f82a62e6d98e3ada1161d863b6d3ca5b83
tygrus <workfile975@gmail.com>
2025-03-02 23:01:01 +0800

    fix: 更新 Vite 配置的 base 屬性，將其設置為根目錄

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-porfolio/', // 設定 base 屬性
+  base: '/', // 設定 base 屬性
   publicDir: 'public', // 添加這一行
   build: {
     assetsInlineLimit: 4096,
